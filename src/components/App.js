cc497e9c9d7966555ba854fcf6e37b13f70e866c
ajay kumar  shah <110763605+ajaykumarshah@users.noreply.github.com>
2022-11-06 20:38:39 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import '../styles/App.css';
 
@@ -28,9 +28,46 @@ const data = {
 }
 const App = () => {
 
+  const keys = Object.keys(data);
+  const updated_keys = keys.map((element, index) => {
+    return <option value={element} key={element + index}>{element}</option>
+  })
+  let nullvalue = <option value={null}></option>
+
+  const [yearselction, setYearselection] = React.useState("No year selected");
+  let [litags, setLitags] = useState();
+  const setthis = (event) => {
+    if (event.target.value != null) {
+      event.persist();
+
+      setYearselection(() => `Selected year- ${event.target.value}`);
+      let year = event.target.value;
+
+      year = data[year]
+
+
+      let litags1 = year.map((element, index) => <li key={element + index}>{element}</li>);
+      setLitags(() => litags1);
+
+
+    }
+  }
+
   return (
     <div id="main">
-      
+      <select name="" id="card" onChange={setthis}  >
+
+
+        {nullvalue}
+        {updated_keys}
+
+      </select>
+      <div id="selected-year">
+        {yearselction}
+      </div>
+      <ul>
+        {litags}
+      </ul>
     </div>
   )
 }
